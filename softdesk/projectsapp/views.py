3cc974a7f980e79ffafab31f563702d0b4dcedde
John <john.t.singer@gmail.com>
2023-10-02 14:20:06 +0200

    Add commentviewset, optimize queries, update permissions

--- softdesk/projectsapp/views.py
+++ softdesk/projectsapp/views.py
@@ -1,15 +1,13 @@
+from django.shortcuts import get_object_or_404
+from django.db.models import Count, Q
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from django.shortcuts import get_object_or_404
-from django.contrib.auth import get_user_model
 from projectsapp.models import (
     Contributor,
     Project,
     Issue,
-    Comment
 )
 from projectsapp.serializers import (
     ContributorListSerializer,
@@ -24,7 +22,8 @@ from projectsapp.serializers import (
 from projectsapp.permissions import (
     IsAuthor,
     IsDataOwner,
-    IsContributor
+    IsContributor,
+    IsProjectCreator
 )
 
 
@@ -46,7 +45,24 @@ class ProjectViewSet(MultipleSerializerMixin, ModelViewSet):
     permission_classes = [IsAuthenticated, IsContributor]
 
     def get_queryset(self):
-        return self.request.user.projects.all()
+        if self.action == 'list':
+            return Project.objects.all().select_related('author').only(
+                'id',
+                'name',
+                'description',
+                'type',
+                'author__username'
+            )
+        return Project.objects.annotate(
+            open_issues_count=Count(
+                'issues',
+                filter=~Q(issues__status='Finished')
+            ),
+            closed_issues_count=Count(
+                'issues',
+                filter=Q(issues__status='Finished')
+            )
+        ).select_related('author')
 
     def get_permissions(self):
         """
@@ -77,36 +93,47 @@ class ProjectViewSet(MultipleSerializerMixin, ModelViewSet):
             }
         )
 
-    @action(
-        methods=['post'],
-        detail=True,
-        url_path='add-contributor',
-        url_name='add_contributor',
-        serializer_class=AddContributorSerializer
-    )
-    def add_contributor(self, request, pk=None):
-        serializer = self.serializer_class(
-            data=request.data
-        )
-        if serializer.is_valid():
-            serializer.save(project=self.get_object())
-            return Response(
-                {'status': 'Contributor added.'}
-            )
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST)
 
-    @action(
-        methods=['post'],
-        detail=True,
-        url_path='remove-contributor',
-        url_name='remove_contributor',
-        serializer_class=AddContributorSerializer
-    )
-    def remove_contibutor(self, request, pk=None):
-        project = self.get_object()
-        if project.author.id == int(request.data['user']):
+class ContributorViewSet(MultipleSerializerMixin, ModelViewSet):
+    serializer_class = ContributorListSerializer
+    detail_serializer_class = ContributorDetailSerializer
+    add_serilaizer_class = AddContributorSerializer
+    permission_classes = [IsAuthenticated]
+    http_method_names = ['get', 'post', 'delete', 'head', 'options']
+
+    def get_queryset(self):
+        return get_object_or_404(
+            self.request.user.projects,
+            pk=self.kwargs['project_pk']
+        ).contributor_set.select_related('user')
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return self.add_serilaizer_class
+        else:
+            return super().get_serializer_class()
+
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of
+        permissions that this view requires.
+        """
+        if self.action in (
+            'destroy',
+            'update',
+            'partial_update',
+            'create'
+        ):
+            permission_classes = (
+                self.permission_classes + [IsProjectCreator]
+            )
+        else:
+            permission_classes = self.permission_classes
+        return [permission() for permission in permission_classes]
+
+    def destroy(self, request, *args, **kwargs):
+        contributor = self.get_object()
+        if contributor.pk == contributor.project.author.pk:
             return Response(
                 {
                     'detail':
@@ -115,34 +142,29 @@ class ProjectViewSet(MultipleSerializerMixin, ModelViewSet):
                 },
                 status=status.HTTP_400_BAD_REQUEST,
             )
-        if not project.contributors.filter(
-            id=int(request.data['user'])
-        ).exists():
-            return Response(
-                {
-                    "user": [
-                        f"Invalid pk {request.data['user']}"
-                        " - Contributor does not exist."
-                    ]
-                },
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-        self.get_object().contributors.remove(
-            request.data['user']
-        )
+        self.perform_destroy(contributor)
         return Response(
-            {'status': 'Contributor removed.'}
+            {"detail: Contributor removed"},
+            status=status.HTTP_200_OK
+        )
+
+    def perform_create(self, serializer):
+        serializer.save(
+            project=Project.objects.get(pk=self.kwargs['project_pk'])
         )
 
 
 class IssueViewSet(MultipleSerializerMixin, ModelViewSet):
     serializer_class = IssueListSerializer
     detail_serializer_class = IssueDetailSerializer
-    permission_classes = [IsAuthenticated, IsContributor]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        return Issue.objects.filter(
-            project=self.kwargs['project_pk']
+        return get_object_or_404(
+            self.request.user.projects,
+            pk=self.kwargs['project_pk']
+        ).issues.annotate(
+            comments_count=Count('comments')
         )
 
     def get_permissions(self):
@@ -155,7 +177,9 @@ class IssueViewSet(MultipleSerializerMixin, ModelViewSet):
             'update',
             'partial_update',
         ):
-            permission_classes = self.permission_classes + [IsAuthor]
+            permission_classes = (
+                self.permission_classes + [IsAuthor | IsProjectCreator]
+            )
         else:
             permission_classes = self.permission_classes
         return [permission() for permission in permission_classes]
@@ -164,4 +188,41 @@ class IssueViewSet(MultipleSerializerMixin, ModelViewSet):
         serializer.save(
             author=self.request.user,
             project=Project.objects.get(pk=self.kwargs['project_pk'])
+        )
+
+
+class CommentViewSet(ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return get_object_or_404(
+            get_object_or_404(
+                self.request.user.projects.select_related('author'),
+                pk=self.kwargs['project_pk']
+            ).issues,
+            pk=self.kwargs['issue_pk']
+        ).comments.select_related('author')
+
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of
+        permissions that this view requires.
+        """
+        if self.action in (
+            'destroy',
+            'update',
+            'partial_update',
+        ):
+            permission_classes = (
+                self.permission_classes + [IsAuthor | IsProjectCreator]
+            )
+        else:
+            permission_classes = self.permission_classes
+        return [permission() for permission in permission_classes]
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            issue=Issue.objects.get(pk=self.kwargs['issue_pk'])
         )
