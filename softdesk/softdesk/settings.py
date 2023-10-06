5a0f397ef611be1192afb30dba4b59dd9b027d34
John <john.t.singer@gmail.com>
2023-10-06 16:20:29 +0200

    Change pagination page size from 5 to 10

--- softdesk/softdesk/settings.py
+++ softdesk/softdesk/settings.py
@@ -140,7 +140,7 @@ AUTH_USER_MODEL = 'authentication.User'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 5,
+    'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
