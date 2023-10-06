bc7d466acc0474ca297a6380f1fcafa60a2b6251
John <john.t.singer@gmail.com>
2023-10-06 14:59:32 +0200

    Change access token lifetime from 30 days to 7 days

--- softdesk/softdesk/settings.py
+++ softdesk/softdesk/settings.py
@@ -153,6 +153,6 @@ if DEBUG:
 
 # Simple JWT
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
 }
